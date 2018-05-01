11669848f22a744b35896092eeebdb71b452fc3c
Source <source@kosmisk.dk>
2018-05-01 21:08:18 +0200

    source - updated doc

--- expanding/source.py
+++ expanding/source.py
@@ -1,9 +1,16 @@
-"""
-"""
-
 
 class At(object):
+    """
+    Location object
+    """
     def __init__(self, source, line=None, pos=None):
+        """
+        Construct a location object
+
+        :param source: the filename
+        :param line: line number
+        :param pos: character on line
+        """
         self.source = source
         self.line = line
         self.pos = pos
@@ -16,7 +23,8 @@ class At(object):
 
 
 class Reader(object):
-    """Buffered reader with location
+    """
+    (Line) buffered reader with location
     """
 
     _QUOTED = {
@@ -25,9 +33,15 @@ class Reader(object):
         "t": "\t"
     }
 
-    def __init__(self, source, source_name="<UNKNOWN>"):
+    def __init__(self, source, name="<UNKNOWN>"):
+        """
+        Construct a reader
+
+        :param source: input file handle
+        :param name: name of source
+        """
         self._source = source
-        self._source_name = source_name
+        self._source_name = name
         self._buffer = []
         self._line = 0
         self._real_line = 0
@@ -35,7 +49,12 @@ class Reader(object):
         self._eof = False
         self._read_line()
 
-    def _read_line(self):
+    def _read_line(self) -> None:
+        """
+        Add a line to the buffer
+
+        Remove old lines, that are no longer needed
+        """
         if not self.eof():
             line = self._source.readline()
             if line is "":
@@ -49,6 +68,11 @@ class Reader(object):
                 self._pos = 1
 
     def get(self) -> str:
+        """
+        Get a character from the input
+
+        :return: character (str) or None of at end of file
+        """
         if self.eof():
             return None
         c = self._buffer[self._line][self._pos]
@@ -62,6 +86,13 @@ class Reader(object):
         return c
 
     def get_quoted(self) -> str:
+        """
+        Read a backquoted value from  input
+
+        typical \\ \r \t \n \octal \uhex or $
+        :return: character
+        :raises: Exception if  EOF is encountered
+        """
         c = self.get()
         if c is None:
             raise Exception("Unexpected EOF - dangling quote")
@@ -80,6 +111,9 @@ class Reader(object):
         return c
 
     def unget(self) -> None:
+        """
+        Roll a character back, in the input
+        """
         self._pos = self._pos - 1
         if self._pos <= 0:
             if self._line == 0:
@@ -88,12 +122,22 @@ class Reader(object):
             self._pos = len(self._buffer[self._line]) - 1
 
     def eof(self) -> bool:
+        """
+        Is the input consumed
+
+        :return: at end of file
+        """
         return self._eof and (
                 self._line >= len(self._buffer) or  # Special for empty input
                 (self._line + 1 == len(self._buffer) and
                  self._pos == len(self._buffer[self._line])))
 
     def at(self) -> str:
+        """
+        The current location in the input
+
+        :return: At (location) object
+        """
         if self.eof():
             return At(self._source_name + ":EOF")
         return At(self._source_name, self._buffer[self._line][0], self._pos)
