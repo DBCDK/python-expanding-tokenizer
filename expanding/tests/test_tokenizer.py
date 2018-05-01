f5c57503b6b7b15a40932d2716e388600d36ae23
Source <source@kosmisk.dk>
2018-05-01 20:28:10 +0200

    Expansion rename - Multiple types of whitespace tokens

--- expanding/tests/test_tokenizer.py
+++ expanding/tests/test_tokenizer.py
@@ -2,14 +2,14 @@ from io import StringIO
 from unittest import TestCase
 
 from expanding.source import At, Reader
-from expanding.tokenizer import Token, TokenType, Tokenizer
+from expanding.tokenizer import *
 from expanding.variable import EnvironmentVariable
 
 at = At("Unknown", 1, 2)
 
 
-def make_tokenizer(text, **kwargs):
-    return Tokenizer(Reader(StringIO(text)), EnvironmentVariable(kwargs))
+def make_tokenizer(text, whitespace=TokenWhitespace.NEWLINE, **kwargs):
+    return Tokenizer(Reader(StringIO(text)), EnvironmentVariable(kwargs), whitespace=whitespace)
 
 
 class TestToken(TestCase):
@@ -28,21 +28,25 @@ class TestToken(TestCase):
 
 class TestTokenizer(TestCase):
 
-    def test_tokens_are(self):
+    def test_tokens_are_word(self):
         self.assertTrue(make_tokenizer("abc \n").tokens_are(TokenType.TEXT))
         self.assertTrue(make_tokenizer("abc \n").tokens_are(TokenType.WORD))
 
+    def test_tokens_are_single_quote(self):
         self.assertTrue(make_tokenizer("'abc' \n").tokens_are(TokenType.TEXT))
         self.assertTrue(make_tokenizer("'abc' \n").tokens_are(TokenType.WORD))
 
+    def test_tokens_are_expanded(self):
         output = []
         self.assertTrue(make_tokenizer(" $FOO ", FOO="a b").tokens_are(TokenType.TEXT, output=output))
         self.assertEqual(1, len(output))
         self.assertEqual("a b", output[0].content())
 
+    def test_tokens_are_double_quote(self):
         self.assertTrue(make_tokenizer('"abc"').tokens_are(TokenType.TEXT))
         self.assertTrue(make_tokenizer('"abc"').tokens_are(TokenType.WORD))
 
+    def test_tokens_are_double_quote_expanded(self):
         output = []
         self.assertTrue(make_tokenizer('foo = "abc $($ID) def"\n', ID="123")
                         .tokens_are(TokenType.WORD, TokenType.EQ, TokenType.TEXT, TokenType.NEWLINE, output=output))
@@ -51,6 +55,7 @@ class TestTokenizer(TestCase):
         self.assertTrue(tokenizer.tokens_are(TokenType.EOF))
         self.assertTrue(tokenizer.tokens_are(TokenType.EOF))
 
+    def test_tokens_are_section_and_comment(self):
         output = []
         self.assertTrue(make_tokenizer('; bah \n[fool] #abc\n  \n', ID="123")
                         .tokens_are(TokenType.SECTION, TokenType.NEWLINE, TokenType.EOF, output=output))
@@ -60,3 +65,56 @@ class TestTokenizer(TestCase):
         self.assertRaises(Exception, make_tokenizer('[fool').tokens_are, TokenType.SECTION)
         self.assertRaises(Exception, make_tokenizer('\'fool').tokens_are, TokenType.TEXT)
         self.assertRaises(Exception, make_tokenizer('"fool').tokens_are, TokenType.TEXT)
+
+    def test_whitespace_none(self):
+        tzr = make_tokenizer("  \n   foo bar", whitespace=TokenWhitespace.NONE);
+        output = []
+
+        self.assertTrue(tzr.tokens_are(TokenType.TEXT, output=output))
+        self.assertEqual(1, len(output))
+        self.assertEqual("foo", output[0].content())
+        self.assertTrue(tzr.tokens_are(TokenType.TEXT, TokenType.EOF))
+
+    def test_whitespace_newline(self):
+        tzr = make_tokenizer("  \n   foo bar", whitespace=TokenWhitespace.NEWLINE);
+        output = []
+        self.assertTrue(tzr.tokens_are(TokenType.NEWLINE, TokenType.TEXT, output=output))
+        self.assertEqual(2, len(output))
+        self.assertEqual("foo", output[1].content())
+        self.assertTrue(tzr.tokens_are(TokenType.TEXT, TokenType.EOF))
+
+    def test_whitespace_newline_match_optional_whitespace(self):
+        tzr = make_tokenizer("  \n   foo bar", whitespace=TokenWhitespace.NEWLINE);
+        output = []
+        self.assertTrue(tzr.tokens_are(TokenType.OPTIONAL_WHITESPACE, TokenType.TEXT, output=output))
+        self.assertEqual(1, len(output))
+        self.assertEqual("foo", output[0].content())
+        self.assertTrue(tzr.tokens_are(TokenType.TEXT, TokenType.EOF))
+
+    def test_whitespace_whitespace(self):
+        tzr = make_tokenizer("  \n   foo bar", whitespace=TokenWhitespace.WHITESPACE);
+        output = []
+        self.assertFalse(tzr.tokens_are(TokenType.NEWLINE, TokenType.TEXT, output=output))
+        self.assertTrue(tzr.tokens_are(TokenType.WHITESPACE, TokenType.TEXT, output=output))
+        self.assertEqual(2, len(output))
+        self.assertEqual("foo", output[1].content())
+        self.assertTrue(tzr.tokens_are(TokenType.OPTIONAL_WHITESPACE, TokenType.TEXT, TokenType.EOF))
+
+    def test_whitespace_both(self):
+        tzr = make_tokenizer("  \n   foo bar", whitespace=TokenWhitespace.BOTH);
+        output = []
+        self.assertFalse(tzr.tokens_are(TokenType.NEWLINE, TokenType.TEXT, output=output))
+        self.assertTrue(tzr.tokens_are(TokenType.WHITESPACE, TokenType.NEWLINE, TokenType.WHITESPACE, TokenType.TEXT, output=output))
+        self.assertEqual(4, len(output))
+        self.assertEqual("foo", output[3].content())
+        self.assertTrue(tzr.tokens_are(TokenType.OPTIONAL_WHITESPACE, TokenType.TEXT, TokenType.EOF))
+
+    def test_whitespace_both_optional(self):
+        tzr = make_tokenizer("  \n   foo'bar'", whitespace=TokenWhitespace.BOTH);
+        output = []
+        self.assertTrue(tzr.tokens_are(TokenType.OPTIONAL_WHITESPACE, TokenType.TEXT, output=output))
+        self.assertEqual(1, len(output))
+        self.assertEqual("foo", output[0].content())
+        self.assertTrue(tzr.tokens_are(TokenType.OPTIONAL_WHITESPACE, TokenType.TEXT, TokenType.EOF))
+
+
--- expanding/tokenizer.py
+++ expanding/tokenizer.py
@@ -1,15 +1,23 @@
 import re
+from distutils.command.config import config
 from enum import Enum
 from io import StringIO
 from typing import TypeVar, List
 
-from expanding.expand import Expanding
+from expanding.expand import Expansion
 from expanding.source import Reader, At
 from expanding.variable import EnvironmentVariable, Variable
 
 
 class TokenType(Enum):
-    # Could be produced synthetically, zbut then completion is missing
+    """
+Type of token
+
+Some are synthetic (groups) ie. matches multiple base tokens
+Some are synthetic (content) ie. matches TEXT with certain properties (single word / number)
+Some are matchers (OPTIONAL_*) ie. matches a list of a certain type
+    """
+    # Could be produced synthetically, but then completion is missing
     TEXT = 'TEXT'
     EQ = 'EQ'
     DOT = 'DOT'
@@ -24,6 +32,7 @@ class TokenType(Enum):
     SEMICOLON = 'SEMICOLON'
     SECTION = 'SECTION'
     NEWLINE = 'NEWLINE'
+    WHITESPACE = 'WHITESPACE'
     ADD = 'ADD'
     SUB = 'SUB'
     MUL = 'MUL'
@@ -37,26 +46,75 @@ class TokenType(Enum):
     EXCLAMATION = 'EXCLAMATION'
     EOF = 'EOF'
     WORD = 'WORD'  # Synthetic: TEXT without whitespace
-    NUMBER = 'NUMBER'  # Synthetic: TEXT containing hex,octal or decimal number, optionally negative
+    NUMBER = 'NUMBER'  # Synthetic: TEXT containing hex, octal or decimal number, optionally negative
     EOL = 'EOL'  # Synthetic: EOF or NEWLINE
+    ANY_WHITESPACE = 'ANY_WHITESPACE'  # Synthetic: whitespace or newline
+    ANY = 'ANY'  # Synthetic: wildcard
+    OPTIONAL_SPACE = 'OPTIONAL_SPACE'  # Matches 0-n whitespace, not outputed
+    OPTIONAL_WHITESPACE = 'OPTIONAL_WHITESPACE'  # Matches 0-n whitespace or newline, not outputed
+
+
+class TokenWhitespace(Enum):
+    """How are whitespaces processed by Tokenizer"""
+    # Could be produced synthetically, but then completion is missing
+    NONE = 'NONE' """Skip all whitespace"""
+    NEWLINE = 'NEWLINE' """Produce only newline tokens"""
+    WHITESPACE = 'WHITESPACE' """Produces whitespace tokens (can contain newlines)"""
+    BOTH = 'BOTH' """Produces both newline and whitespace tokens (whitespace will not contain newlines)"""
 
 
 class Token(object):
+    """
+Container for a token
+
+Implements location, type and content
+Also has matchers for token types
+    """
     _IS_NUMBER = re.compile('^(?:(?:[-+]?)(?:0[xX][0-9a-fA-F]+)|(?:[1-9][0-9]*)|(?:0[0-7]*))$', re.S | re.U)
     _IS_WORD = re.compile('^\\S+$', re.S | re.U)
 
     def __init__(self, at: At, token_type: TokenType, content: str) -> TypeVar('Token'):
+        """
+        Token class contructor
+
+        :param at: Location of token
+        :param token_type: type
+        :param content: content string
+        :returns: new object
+        """
         self._at = at
         self._token_type = token_type
         self._content = content
 
     def at(self) -> At:
+        """
+        Get location of token
+
+        :returns: At object where this token starts
+        """
         return self._at
 
     def content(self) -> str:
+        """
+        Get string with token content, most useful whendealing with TEXT type tokens
+
+        :returns: content string
+        """
         return self._content
 
     def is_a(self, wanted_type: TokenType) -> bool:
+        """
+        Is the token of a given type
+
+        Handles synthetic types such as, WORD, NUMBER, EOL
+
+        :param wanted_type: type
+        :returns: true if type is matched
+        """
+        if wanted_type is TokenType.ANY:
+            return True
+        if wanted_type is TokenType.ANY_WHITESPACE:
+            return self._token_type is TokenType.NEWLINE or self._token_type is TokenType.WHITESPACE
         if wanted_type is TokenType.EOL:
             return self._token_type is TokenType.NEWLINE or self._token_type is TokenType.EOF
         if wanted_type is TokenType.NUMBER:
@@ -70,6 +128,13 @@ class Token(object):
 
 
 class Tokenizer(object):
+    """
+Tokenizer
+
+Primary purpose of this package is
+ * expanding variables / expressions and
+ * patten-matching content from  a input source
+"""
     _SINGLE_CHARACTER_TOKENS = {
         '=': TokenType.EQ,
         '.': TokenType.DOT,
@@ -97,57 +162,130 @@ class Tokenizer(object):
 
     @staticmethod
     def ini_from_file(filename: str) -> TypeVar('Tokenizer'):
+        """
+        Create a Tokenizer from a file, for parsing ini files
+
+        The file is read into memory - inefficient when dealing with large files.
+
+        :param filename: path of file
+        :returns: new object
+        """
         with open(filename, 'r') as f:
             content = f.read()
             reader = Reader(source=StringIO(content), source_name=filename)
-            return Tokenizer(reader=reader, variable=EnvironmentVariable(), newline=True, single_tokens="=")
+            return Tokenizer(reader=reader, variable=EnvironmentVariable(), whitespace=TokenWhitespace.NEWLINE, single_tokens="=")
 
     @staticmethod
     def full_from_file(filename: str) -> TypeVar('Tokenizer'):
+        """
+        Create a Tokenizer from a file, for parsing any files
+
+        The file is read into memory - inefficient when dealing with large files.
+
+        :param filename: path of file
+        :returns: new object
+        """
         with open(filename, 'r') as f:
             content = f.read()
             reader = Reader(source=StringIO(content), source_name=filename)
-            return Tokenizer(reader=reader, variable=EnvironmentVariable(), newline=True,
+            return Tokenizer(reader=reader, variable=EnvironmentVariable(), whitespace=TokenWhitespace.BOTH,
                              single_tokens="".join(Tokenizer._SINGLE_CHARACTER_TOKENS.keys()))
 
-    def __init__(self, reader: Reader, variable: Variable = EnvironmentVariable(), newline: bool = True,
+    def __init__(self, reader: Reader, variable: Variable = EnvironmentVariable(),
+                 whitespace: TokenWhitespace = TokenWhitespace.NEWLINE,
                  single_tokens: str = "=") -> TypeVar('Tokenizer'):
+        """
+        Tokenizer constructor
+
+        :param reader: the file source
+        :param variable: the variable expander (defaults to Environment)
+        :param whitespace: should newlines be tokens
+        :param single_tokens: String of chars thet should be their own tokens
+                              see _SINGLE_CHARACTER_TOKENS for known tokens
+        :returns: new object
+        """
         self._variable = variable
         self._reader = reader
-        self._newline = newline
-        self.expander = Expanding(reader, variable)
+        if whitespace is TokenWhitespace.BOTH:
+            self._handle_whitespace = self._handle_whitespace_both
+        elif whitespace is TokenWhitespace.NEWLINE:
+            self._handle_whitespace = self._handle_whitespace_newline
+        elif whitespace is TokenWhitespace.WHITESPACE:
+            self._handle_whitespace = self._handle_whitespace_whitespace
+        else:
+            self._handle_whitespace = self._handle_whitespace_none
+        self.expander = Expansion(reader, variable)
         self._tokens = []
         self._single_tokens = dict(
             [(x, self._SINGLE_CHARACTER_TOKENS[x]) for x in self._SINGLE_CHARACTER_TOKENS.keys() if x in single_tokens])
         self._break_chars = ''.join(self._single_tokens.keys()) + "[]$;#'" + '"'
 
     def peek_token(self) -> Token:
+        """
+        Look at the next token, mostly for error reporting, when unable to match a token sequence
+
+        :returns: next token
+        """
         if not self._tokens:
             self._next_token()
         return self._tokens[0]
 
     def tokens_are(self, *args: TokenType, output: List[Token] = []) -> List[Token]:
-        for i in range(0, len(args)):
-            if len(self._tokens) <= i:
-                self._next_token()
-            if not self._tokens[i].is_a(args[i]):
-                return None
-        for token in self._tokens[0:len(args)]:
+        """
+        Match the inut for a list of tokens.
+
+        :param args: list of tokentypes to match
+        :param output: where to put the matched token
+                       (only put is entire token list matches)
+        :returns: sane as output or None if no match is made
+        """
+        taken = []
+        i = 0
+        for arg in args:
+            # print("match:", arg)
+            while True:
+                while len(self._tokens) == i:
+                    self._next_token()
+                if arg is TokenType.OPTIONAL_SPACE:
+                    if self._tokens[i].is_a(TokenType.WHITESPACE):
+                        # print("space:", str(self._tokens[i]))
+                        i = i + 1
+                        continue
+                elif arg is TokenType.OPTIONAL_WHITESPACE:
+                    if self._tokens[i].is_a(TokenType.ANY_WHITESPACE):
+                        # print("whitespace:", str(self._tokens[i]))
+                        i = i + 1
+                        continue
+                elif not self._tokens[i].is_a(arg):
+                    # print("not", str(self._tokens[i]))
+                    return None
+                else:
+                    # print("ok", str(self._tokens[i]))
+                    taken.append(self._tokens[i])
+                    i = i + 1
+                break
+        # print([str(t) for t in taken])
+
+        for token in taken:
             output.append(token)
-        self._tokens = self._tokens[len(args):]
+        self._tokens = self._tokens[i:]
         return output
 
     def _next_token(self) -> None:
+        """
+        Construct a new token, and puts it in the token list
+
+        :raises Exception: if input is invalid
+        """
         while True:
             at = self._reader.at()
             c = self._reader.get()
             if c is None:
                 self._tokens.append(Token(at, TokenType.EOF, ''))
                 return
-            if self._newline and c is "\n":
-                self._tokens.append(Token(at, TokenType.NEWLINE, c))
-                return
             if str.isspace(c):
+                if self._handle_whitespace(at, c):
+                    return
                 continue
             if c is '#' or c is ';':
                 while True:
@@ -184,7 +322,92 @@ class Tokenizer(object):
                 content.write(c)
             self._tokens.append(Token(at, TokenType.TEXT, content.getvalue()))
 
+    def _handle_whitespace_none(self, at, c) -> False:
+        """
+        Eat all whitespace in source
+
+        :param at: required by interface
+        :param c: required by interface
+        :return False: Doesn't produce a token
+        """
+        while True:
+            if c is None:
+                return False
+            if not str.isspace(c):
+                self._reader.unget()
+                return False
+            c = self._reader.get()
+
+    def _handle_whitespace_newline(self, at, c) -> bool:
+        """
+        Constructs a newline token if a newline is encountered in whitespace block
+
+        :param at: Needed for new token
+        :param c: first whitespace character
+        :return bool: if a newline is encountered
+        """
+        while True:
+            if c is None:
+                return False
+            if c is "\n":
+                self._tokens.append(Token(at, TokenType.NEWLINE, c))
+                return True
+            if not str.isspace(c):
+                self._reader.unget()
+                return False
+            c = self._reader.get()
+
+    def _handle_whitespace_whitespace(self, at, c) -> True:
+        """
+        Constructs a whitespace token with optional newlines in it
+
+        :param at: Needed for new token
+        :param c: first whitespace character
+        :return True: will always produce a token
+        """
+        content = StringIO()
+        while True:
+            if c is None:
+                self._tokens.append(Token(at, TokenType.WHITESPACE, content.getvalue()))
+                return True
+            if not str.isspace(c):
+                self._reader.unget()
+                self._tokens.append(Token(at, TokenType.WHITESPACE, content.getvalue()))
+                return True
+            content.write(c)
+            c = self._reader.get()
+
+    def _handle_whitespace_both(self, at, c) -> True:
+        """
+        Constructs a newline token or a whitespace token
+
+        :param at: Needed for new token
+        :param c: first whitespace character
+        :return True: will always produce a token
+        """
+        if c is "\n":
+            self._tokens.append(Token(at, TokenType.NEWLINE, c))
+            return True
+        content = StringIO()
+        while True:
+            if c is None:
+                self._tokens.append(Token(at, TokenType.WHITESPACE, content.getvalue()))
+                return True
+            if not str.isspace(c) or c is "\n":
+                self._reader.unget()
+                self._tokens.append(Token(at, TokenType.WHITESPACE, content.getvalue()))
+                return True
+            content.write(c)
+            c = self._reader.get()
+
     def _read_single_quote(self, at) -> None:
+        """
+        Constructs a token from a single quote text, and puts it into the token list.
+
+        Input should be positioned after 1st quote
+
+        :raises Exception: On unexpected eof
+        """
         content = StringIO()
         while True:
             c = self._reader.get()
@@ -200,6 +423,14 @@ class Tokenizer(object):
             content.write(c)
 
     def _read_double_quote(self, at) -> None:
+        """
+        Constructs a token from a double quote text, and puts it into the token list.
+
+        Input should be positioned after 1st quote.
+        \\ escapes are expanded and variables are expanded
+
+        :raises Exception: On unexpected eof, invalid quote or variable
+        """
         content = StringIO()
         while True:
             a = self._reader.at()
@@ -217,6 +448,13 @@ class Tokenizer(object):
             content.write(c)
 
     def _read_section(self, at) -> None:
+        """
+        Constructs a token from a section, and puts it into the token list.
+
+        Input should be positioned after [.
+
+        :raises Exception: On unexpected eof or whitespace
+        """
         content = StringIO()
         while True:
             c = self._reader.get()
@@ -228,3 +466,4 @@ class Tokenizer(object):
             if str.isspace(c):
                 raise Exception("Whitespace is not allowed in section at: %s" % at)
             content.write(c)
+
